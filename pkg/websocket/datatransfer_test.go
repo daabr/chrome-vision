6a031a06144e0925a3e0e4ed38172483f84e3b48
Daniel Abraham <daab@google.com>
2021-05-16 21:35:47 -0700

    Initial version of websocket package

---
+++ pkg/websocket/datatransfer_test.go
@@ -0,0 +1,256 @@
+package websocket_test
+
+import (
+	"encoding/binary"
+	"io"
+	"net"
+	"testing"
+
+	"github.com/daabr/chrome-vision/pkg/websocket"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestReadErrors(t *testing.T) {
+	tests := []struct {
+		desc string
+		b    []byte
+	}{
+		{
+			"reserved bits",
+			[]byte{0x70},
+		},
+		{
+			"invalid opcode",
+			[]byte{0x0f},
+		},
+		{
+			"mask bit",
+			[]byte{0x80, 0x80},
+		},
+	}
+	for i, tc := range tests {
+		server, client := net.Pipe()
+		conn := websocket.NewConn(client)
+		defer server.Close()
+		defer client.Close()
+
+		go func() {
+			server.Write(tc.b)
+		}()
+
+		got, err := conn.Read()
+		if err == nil {
+			t.Errorf("TC %d: Conn.Read() = %#v, want %s error", i, got, tc.desc)
+		}
+	}
+}
+
+func TestReadSingleEmptyFrame(t *testing.T) {
+	server, client := net.Pipe()
+	conn := websocket.NewConn(client)
+	defer server.Close()
+	defer client.Close()
+
+	go func() {
+		b := []byte{0x81, 0x00}
+		server.Write(b)
+	}()
+
+	got, err := conn.Read()
+	if err != nil {
+		t.Fatalf("Conn.Read(); got unexpected error: %v", err)
+	}
+	if len(got) > 0 {
+		t.Errorf("Conn.Read() = %#v, want %#v", got, []byte{})
+	}
+}
+
+func TestReadThreeFrames(t *testing.T) {
+	server, client := net.Pipe()
+	conn := websocket.NewConn(client)
+	defer server.Close()
+	defer client.Close()
+
+	go func() {
+		b := []byte{0x01, 0x01, 0xaa, 0x00, 0x02, 0xbb, 0xcc, 0x80, 0x03, 0xdd, 0xee, 0xff}
+		server.Write(b)
+	}()
+
+	got, err := conn.Read()
+	want := []byte{0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff}
+	if err != nil {
+		t.Fatalf("Conn.Read(); got unexpected error: %v", err)
+	}
+	if !cmp.Equal(got, want) {
+		t.Errorf("Conn.Read() = %#v, want %#v", got, want)
+	}
+}
+
+// TODO: test reading two frames with control frames in the middles.
+
+func TestRead1KB(t *testing.T) {
+	server, client := net.Pipe()
+	conn := websocket.NewConn(client)
+	defer server.Close()
+	defer client.Close()
+
+	want := make([]byte, 1024)
+	want[0] = 0x01
+	want[1023] = 0x02
+
+	go func() {
+		extendedLength := make([]byte, 2)
+		binary.BigEndian.PutUint16(extendedLength, uint16(len(want)))
+		b := append([]byte{0x81, 0x7e}, extendedLength...)
+		b = append(b, want...)
+		server.Write(b)
+	}()
+
+	got, err := conn.Read()
+	if err != nil {
+		t.Fatalf("Conn.Read(); got unexpected error: %v", err)
+	}
+	if !cmp.Equal(got, want) {
+		t.Errorf("Conn.Read() = %#v, want %#v", got, want)
+	}
+}
+
+func TestRead1MB(t *testing.T) {
+	server, client := net.Pipe()
+	conn := websocket.NewConn(client)
+	defer server.Close()
+	defer client.Close()
+
+	want := make([]byte, 1024*1024)
+	want[0] = 0x01
+	want[1024*1024-1] = 0x02
+
+	go func() {
+		extendedLength := make([]byte, 8)
+		binary.BigEndian.PutUint64(extendedLength, uint64(len(want)))
+		b := append([]byte{0x81, 0x7f}, extendedLength...)
+		b = append(b, want...)
+		server.Write(b)
+	}()
+
+	got, err := conn.Read()
+	if err != nil {
+		t.Fatalf("Conn.Read(); got unexpected error: %v", err)
+	}
+	if !cmp.Equal(got, want) {
+		t.Errorf("Conn.Read() = %#v, want %#v", got, want)
+	}
+}
+
+func TestWriteSingleEmptyFrame(t *testing.T) {
+	server, client := net.Pipe()
+	conn := websocket.NewConn(client)
+	defer server.Close()
+	defer client.Close()
+
+	go func() {
+		conn.WriteBinary([]byte{})
+	}()
+
+	b := make([]byte, 7)
+	n, err := server.Read(b)
+	if err != nil {
+		t.Fatalf("server.Read(b); got unexpected error: %v", err)
+	}
+	if n != 6 {
+		t.Errorf("server.Read(b) = %d, want 6", n)
+	}
+	if b[0] != 0x82 {
+		t.Errorf("server.Read(b); b[0] = %b, want %b", b, 0x82)
+	}
+	if b[1] != 0x80 {
+		t.Errorf("server.Read(b); b[1] = %b, want %b", b, 0x80)
+	}
+}
+
+func TestWrite1Byte(t *testing.T) {
+	server, client := net.Pipe()
+	conn := websocket.NewConn(client)
+	defer server.Close()
+	defer client.Close()
+
+	go func() {
+		conn.WriteText([]byte{0x00})
+	}()
+
+	// 2 (minimum header) + 4 (masking key) + 1 (payload)
+	// + 1 (to detect unexpected bytes).
+	want := 2 + 4 + 1
+	b := make([]byte, want+1)
+	n, err := server.Read(b)
+	if err != nil {
+		t.Fatalf("server.Read(b); got unexpected error: %v", err)
+	}
+	if n != want {
+		t.Errorf("server.Read(b) = %d, want %d", n, want)
+	}
+	if b[0] != 0x81 { // Fin, text frame.
+		t.Errorf("server.Read(b); b[0] = %b, want %b", b[0], 0x81)
+	}
+	if b[1] != 0x81 { // Mask, payload length 1.
+		t.Errorf("server.Read(b); b[1] = %b, want %b", b[1], 0x81)
+	}
+}
+
+func TestWrite1KB(t *testing.T) {
+	server, client := net.Pipe()
+	conn := websocket.NewConn(client)
+	defer server.Close()
+	defer client.Close()
+
+	go func() {
+		conn.WriteBinary(make([]byte, 1024))
+	}()
+
+	// 2 (minimum header) + 2 (extended length) + 4 (masking key)
+	// + 1024 (payload) + 1 (to detect unexpected bytes).
+	want := 2 + 2 + 4 + 1024
+	b := make([]byte, want+1)
+	n, err := server.Read(b)
+	if err != nil {
+		t.Fatalf("server.Read(b); got unexpected error: %v", err)
+	}
+	if n != want {
+		t.Errorf("server.Read(b) = %d, want %d", n, want)
+	}
+	if b[0] != 0x82 { // Fin, binary frame.
+		t.Errorf("server.Read(b); b[0] = %b, want %b", b[0], 0x82)
+	}
+	if b[1] != 0xfe { // Mask, payload length 126.
+		t.Errorf("server.Read(b); b[1] = %b, want %b", b[1], 0xfe)
+	}
+}
+
+func TestWrite1MB(t *testing.T) {
+	server, client := net.Pipe()
+	conn := websocket.NewConn(client)
+	defer server.Close()
+	defer client.Close()
+
+	go func() {
+		conn.WriteText(make([]byte, 1024*1024))
+	}()
+
+	// 2 (minimum header) + 8 (extended length) + 4 (masking key)
+	// + 1024^2 (payload) + 1 (to detect unexpected bytes).
+	want := 2 + 8 + 4 + (1024 * 1024)
+	b := make([]byte, want+1)
+	n, err := io.ReadAtLeast(server, b, 1000*1000)
+	if err != nil {
+		t.Fatalf("server.Read(b); got unexpected error: %v", err)
+	}
+	if n != want {
+		t.Errorf("server.Read(b) = %d, want %d", n, want)
+	}
+	if b[0] != 0x81 { // Fin, text frame.
+		t.Errorf("server.Read(b); b[0] = %b, want %b", b[0], 0x81)
+	}
+	if b[1] != 0xff { // Mask, payload length 127.
+		t.Errorf("server.Read(b); b[1] = %b, want %b", b[1], 0xff)
+	}
+}
---
+++ pkg/websocket/handshake.go
@@ -0,0 +1,219 @@
+package websocket
+
+import (
+	"bufio"
+	"context"
+	"crypto/rand"
+	"crypto/sha1"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"strings"
+	"time"
+)
+
+const (
+	dialTimeout   = 5 * time.Second
+	websocketGUID = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+)
+
+// Conn defines a WebSocket connection with a buffered I/O interface.
+type Conn struct {
+	nc net.Conn
+	rw *bufio.ReadWriter
+}
+
+func NewConn(c net.Conn) *Conn {
+	rw := bufio.NewReadWriter(bufio.NewReader(c), bufio.NewWriter(c))
+	return &Conn{nc: c, rw: rw}
+}
+
+func (c *Conn) Close() error {
+	return c.nc.Close()
+}
+
+// Handshake initiates a WebSocket connection with "ws://addr/path",
+// based on https://datatracker.ietf.org/doc/html/rfc6455#section-4.1.
+func Handshake(ctx context.Context, addr, path string) (*Conn, error) {
+	// Network connection.
+	d := net.Dialer{Timeout: dialTimeout}
+	netConn, err := d.DialContext(ctx, "tcp", addr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to dial to browser: %v", err)
+	}
+	wsConn := NewConn(netConn)
+
+	// Upgrade request.
+	nonce, err := nonce()
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate a nonce: %v", err)
+	}
+	sendUpgradeRequest(wsConn, addr, path, nonce)
+
+	// Upgrade response.
+	err = receiveUpgradeResponse(wsConn, nonce)
+	if err != nil {
+		return nil, fmt.Errorf("failed to receive WebSocket upgrade response: %v", err)
+	}
+	return wsConn, nil
+}
+
+// Generate a nonce consisting of a randomly selected 16-byte value
+// that has been Base64-encoded.
+func nonce() (string, error) {
+	b := make([]byte, 16)
+	if _, err := io.ReadFull(rand.Reader, b); err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(b), nil
+}
+
+func sendUpgradeRequest(c *Conn, addr, path, nonce string) {
+	// The handshake MUST be a valid HTTP request as specified by [RFC2616].
+	// The method of the request MUST be GET, and the HTTP version MUST	be at least 1.1.
+	// The "Request-URI" part of the request MUST match the /resource name/ defined in
+	// Section 3 (a relative URI) or be an absolute	HTTP/HTTPS URI that, when parsed,
+	// has a /resource name/, /host/, and /port/ that match the corresponding ws/wss URI.
+	fmt.Fprintf(c.nc, "GET %s HTTP/1.1\r\n", path)
+	// The request MUST contain a |Host| header field whose value contains /host/ plus
+	// optionally ":" followed by /port/ (when not using the default port).
+	fmt.Fprintf(c.nc, "Host: %s\r\n", addr)
+	// The request MUST contain an |Upgrade| header field whose value MUST include the
+	// "websocket" keyword.
+	fmt.Fprint(c.nc, "Upgrade: websocket\r\n")
+	// The request MUST contain a |Connection| header field whose value MUST include the
+	// "Upgrade" token.
+	fmt.Fprint(c.nc, "Connection: Upgrade\r\n")
+	// The request MUST include a header field with the name |Sec-WebSocket-Key|.
+	// The value of this header field MUST be a nonce consisting of a randomly selected
+	// 16-byte value that has been Base64-encoded (see Section 4 of [RFC4648]).
+	// The nonce MUST be selected randomly for each connection.
+	fmt.Fprintf(c.nc, "Sec-WebSocket-Key: %s\r\n", nonce)
+
+	// The request MUST include a header field with the name |Origin| [RFC6454] if the
+	// request is coming from a browser client. If the connection is from a non-browser
+	// client, the request MAY include this header field if the semantics of that client
+	// match the use-case described here for browser clients. The value of this header
+	// field is the ASCII serialization of origin of the context in which the code
+	// establishing the connection is running. See [RFC6454] for the details of how this
+	// header field value is constructed.
+
+	// The request MUST include a header field with the name |Sec-WebSocket-Version|.
+	// The value of this header field MUST be 13.
+	fmt.Fprint(c.nc, "Sec-WebSocket-Version: 13\r\n")
+
+	// The request MAY include a header field with the name |Sec-WebSocket-Protocol|. If
+	// present, this value indicates one or more comma-separated subprotocol the client
+	// wishes to speak, ordered by preference. The elements that comprise this value MUST
+	// be non-empty strings with characters in the range U+0021 to U+007E not including
+	// separator characters as defined in [RFC2616] and MUST all be unique strings. The
+	// ABNF for the value of this header field is 1#token, where the definitions of
+	// constructs and rules are as given in [RFC2616].
+
+	// The request MAY include a header field with the name |Sec-WebSocket-Extensions|.
+	// If present, this value indicates the protocol-level extension(s) the client wishes
+	// to speak. The interpretation and format of this header field is described in
+	// Section 9.1.
+
+	// The request MAY include any other header fields, for example, cookies [RFC6265]
+	// and/or authentication-related header fields such as the |Authorization| header
+	// field [RFC2616], which are processed according to documents that define them.
+
+	fmt.Fprint(c.nc, "\r\n")
+}
+
+func receiveUpgradeResponse(c *Conn, nonce string) error {
+	line, err := c.rw.ReadString('\n')
+	if err != nil {
+		return fmt.Errorf("failed to read the response status line: %v", err)
+	}
+	// If the status code received from the server is not 101, the client handles the
+	// response per HTTP [RFC2616] procedures. In particular, the client might perform
+	// authentication if it receives a 401 status code; the server might redirect the
+	// client using a 3xx status code (but clients are not required to follow them),
+	// etc. Otherwise, proceed as follows.
+	if !strings.HasPrefix(line, "HTTP/1.1 101") {
+		return fmt.Errorf("expected status code 101, got %s", strings.TrimSpace(line))
+	}
+	// Parse the headers after the status line.
+	gotUpgrade, gotConnection, gotNonce := false, false, false
+	for line != "" {
+		line, err = c.rw.ReadString('\n')
+		if err != nil {
+			return fmt.Errorf("failed to read a response header line: %v", err)
+		}
+		line = strings.TrimSpace(line)
+		tokens := strings.Split(line, " ")
+		// According to [RFC2616], all header field names in both HTTP requests and
+		// HTTP responses are case-insensitive.
+		switch strings.ToLower(tokens[0]) {
+		// If the response lacks an |Upgrade| header field or the |Upgrade| header field
+		// contains a value that is not an ASCII case-insensitive match for the value
+		// "websocket", the client MUST _Fail the WebSocket Connection_.
+		case "upgrade:":
+			gotUpgrade = true
+			if strings.ToLower(tokens[1]) != "websocket" {
+				return fmt.Errorf("unexpected value in the Upgrade header: %q", tokens[1])
+			}
+		// If the response lacks a |Connection| header field or the |Connection| header
+		// field doesn't contain a token that is an ASCII case-insensitive match for the
+		// value "Upgrade", the client MUST _Fail the WebSocket Connection_.
+		case "connection:":
+			gotConnection = true
+			if strings.ToLower(tokens[1]) != "upgrade" {
+				return fmt.Errorf("unexpected value in the Connection header: %q", tokens[1])
+			}
+		// If the response lacks a |Sec-WebSocket-Accept| header field or the
+		// |Sec-WebSocket-Accept| contains a value other than the Base64-encoded SHA-1 of
+		// the concatenation of the |Sec-WebSocket-Key| (as a string, not base64-decoded)
+		// with the string "258EAFA5-E914-47DA-95CA-C5AB0DC85B11" but ignoring any
+		// leading and trailing whitespace, the client MUST _Fail the WebSocket
+		// Connection_.
+		case "sec-websocket-accept:":
+			gotNonce = true
+			expected, err := expectedKey(nonce)
+			if err != nil {
+				return err
+			}
+			if tokens[1] != expected {
+				e := "unexpected value in the Sec-WebSocket-Accept header"
+				return fmt.Errorf("%s: got %q, expected %q", e, tokens[1], expected)
+			}
+		case "":
+			continue // End of response.
+		default:
+			log.Printf("unexpected WebSocket upgrade response header: %q", line)
+			continue
+		}
+	}
+	// Check we got all the necessary headers.
+	if !gotUpgrade {
+		return errors.New("WebSocket upgrade response lacks an Upgrade header")
+	}
+	if !gotConnection {
+		return errors.New("WebSocket upgrade response lacks a Connection header")
+	}
+	if !gotNonce {
+		return errors.New("WebSocket upgrade response lacks a Sec-WebSocket-Accept header")
+	}
+	return nil
+}
+
+// Generate the "Sec-WebSocket-Accept" header value expected from the server:
+// the Base64-encoded SHA-1 hash of the client-provided Base64 key and the
+// GUID "258EAFA5-E914-47DA-95CA-C5AB0DC85B11".
+func expectedKey(nonce string) (string, error) {
+	h := sha1.New()
+	_, err := h.Write([]byte(nonce))
+	if err != nil {
+		return "", errors.New("failed to hash the server's Sec-WebSocket-Accept header")
+	}
+	_, err = h.Write([]byte(websocketGUID))
+	if err != nil {
+		return "", errors.New("failed to hash the server's Sec-WebSocket-Accept header")
+	}
+	return base64.StdEncoding.EncodeToString(h.Sum(nil)), nil
+}
