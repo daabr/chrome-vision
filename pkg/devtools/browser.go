1c2edc69e8f3442856b85bc617b64e6926676034
Daniel Abraham <daab@google.com>
2021-05-19 23:37:38 -0700

    Tiny fix in the tweaked comment

--- pkg/devtools/browser.go
+++ pkg/devtools/browser.go
@@ -30,8 +30,8 @@ type windowsStderrWriter struct {
 	stderr  *os.File
 }
 
-// Write passes lines from the browser's STDERR to our log file, but also extract
-// and remember the browser's WebSocket address for initialization.
+// Write passes lines from the browser's STDERR to our log file, but also
+// extracts and remembers the browser's WebSocket address for initialization.
 func (w *windowsStderrWriter) Write(b []byte) (n int, err error) {
 	if w.session.wsAddress.read() == "" {
 		re := regexp.MustCompile(`ws://(.+:\d+)(/devtools/browser/[\w-]{36})`)
